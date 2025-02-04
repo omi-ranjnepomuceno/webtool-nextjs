881549cd35cf991246eddad615c9bfa6e9ec3dbb
Randell Janus Nepomuceno <ranj.nepomuceno@smsupermalls.com>
2025-02-04 23:36:59 +0800

    Test prettier

--- src/app/(auth)/_lib/utils.ts
+++ src/app/(auth)/_lib/utils.ts
@@ -26,6 +26,6 @@ export async function verifyToken(token: string) {
 }
 
 export async function getUserDetails() {
-  const { data } = await userDetailsQuery();
+  const data = await userDetailsQuery();
   return data.me;
 }
